ec5aef9520dd7d1480301ead9ab0d1884aca6350
kimty8315 <47734960+kimty8315@users.noreply.github.com>
2020-01-30 19:59:22 +0900

    StockChar class check

--- first_start.py
+++ first_start.py
@@ -3,4 +3,56 @@ instCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
 print(instCpCybos.IsConnect)
 
 instCpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
-print(instCpStockCode.GetCount())
+stockNum = instCpStockCode.GetCount()
+
+instCpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
+print(instCpStockCode.GetData(1,0))
+for i in range(0,10):
+    print(instCpStockCode.GetData(1,i))
+
+for i in range(0,stockNum):
+    if instCpStockCode.GetData(1,i) == 'NAVER':
+        print(instCpStockCode.GetData(0,i))
+        print(instCpStockCode.GetData(1,i))
+        print(i)
+
+# Stock Code to csv file
+instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+codeList = instCpCodeMgr.GetStockListByMarket(1)
+
+kospi ={}
+for code in codeList:
+    name = instCpCodeMgr.CodeToName(code)
+    kospi[code] = name
+
+f = open('C:\\Users\kimty\Desktop\Doing for fun\\new_revenue\kospi_code.csv','w')
+for key, value in kospi.items():
+    f.write("%s,%s\n" % (key,value))
+f.close
+
+instMarketEye = win32com.client.Dispatch("CpSysDib.MarketEye")
+
+instMarketEye.SetInputValue(0, (4, 67, 70, 111))
+instMarketEye.SetInputValue(1, 'A035420')
+
+instMarketEye.BlockRequest()
+# GetData
+print("현재가: ", instMarketEye.GetDataValue(0, 0))
+print("PER: ", instMarketEye.GetDataValue(1, 0))
+print("EPS: ", instMarketEye.GetDataValue(2, 0))
+print("최근분기년월: ", instMarketEye.GetDataValue(3, 0))
+
+instStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
+
+instStockChart.SetInputValue(0, "A035420")
+instStockChart.SetInputValue(1, ord('2'))
+instStockChart.SetInputValue(4, 10)
+instStockChart.SetInputValue(5, 5)
+instStockChart.SetInputValue(6, ord('D'))
+instStockChart.SetInputValue(9, ord('1'))
+
+instStockChart.BlockRequest()
+
+numData = instStockChart.GetHeaderValue(3)
+for i in range(numData):
+    print(instStockChart.GetDataValue(0, i))
